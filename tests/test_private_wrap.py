324835c908808f9e7a0fd768f3b22225dadc733c
Locked-chess-official <13140752715@163.com>
2026-03-17 10:30:28 +0800

    optimize the code in test and change pyi

--- tests/test_private_wrap.py
+++ tests/test_private_wrap.py
@@ -11,9 +11,13 @@ class MyWrap:
         setattr(self, f"func{self.index}", func)
         return self
 
-    def get(self, index, instance, owner):
-        func =  getattr(self, f"func{index}")
-        return func.__get__(instance, owner)
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        def wrapper(index, *args, **kwargs):
+            func = getattr(self, f"func{index}")
+            return func.__get__(instance, owner)(*args, **kwargs)
+        return wrapper
 
 class MyClass(private_attribute.PrivateAttrBase):
     __private_attrs__ = ["_secret1", "_secret2", "_secret3"]
@@ -37,9 +41,9 @@ class MyClass(private_attribute.PrivateAttrBase):
 class TestPrivateWrap(unittest.TestCase):
     def test_private_wrap(self):
         a = MyClass(1, 2, 3)
-        self.assertEqual(a.get_secret.get(1, a, MyClass)(), 1)
-        self.assertEqual(a.get_secret.get(2, a, MyClass)(), 2)
-        self.assertEqual(a.get_secret.get(3, a, MyClass)(), 3)
+        self.assertEqual(a.get_secret(1), 1)
+        self.assertEqual(a.get_secret(2), 2)
+        self.assertEqual(a.get_secret(3), 3)
         with self.assertRaises(AttributeError):
             a._secret1
         with self.assertRaises(AttributeError):
